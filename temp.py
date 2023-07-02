3b1788da5ecacb616331a3be923f634db90c23e1
eeicc <59785645+eeicc@users.noreply.github.com>
2023-07-03 00:48:45 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,24 @@
+#封裝:利用公開的方法存取私有屬性
+class Employee:
+    def __init__(self):
+        self.__id = 0
+        self.__name = ''
+    # def set_id(self,value):
+    #     if value <0:
+    #         raise ValueError('ID輸入錯誤')
+    #     self.__id=value
+    # def get_id(self):
+    #     return self.__id
+    # id=property(fget=get_id,fset=set_id)
+    
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self,value):
+        if value < 0:    
+            raise ValueError('ID輸入錯誤')
+        self.__id=value
+emp=Employee()
+emp.id=1
+print(emp.id)
