4bc0b8b8916ffcbfc5fdc05210602e21d668a177
eeicc <59785645+eeicc@users.noreply.github.com>
2023-07-03 21:58:31 +0800

    Update 封裝.py

--- 封裝.py
+++ 封裝.py
@@ -3,16 +3,6 @@ class Employee:
     def __init__(self):
         self.__id = 0
         self.__name = ''
-    #則一顯示 8~14 or 16~23
-    
-    # def set_id(self,value):
-    #     if value <0:
-    #         raise ValueError('ID輸入錯誤')
-    #     self.__id=value
-    # def get_id(self):
-    #     return self.__id
-    # id=property(fget=get_id,fset=set_id)
-    
     @property
     def id(self):
         return self.__id
